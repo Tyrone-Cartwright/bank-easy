39508ce4632460cc1921539717d0a612367d60b4
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2021-06-03 09:24:25 -0400

    Navbar nearly finished
    Navbar missing functionality but is responsive and styled per design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>HELLO</h1>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
